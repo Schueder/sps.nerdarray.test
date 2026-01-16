70d4dc7336a8bdb026e98a96f933e19524e5a5a9
Neels Schueder <n.schueder@gmx.de>
2026-01-16 21:35:39 +0100

    fixed url issue 2

---
+++ .eleventy copy.js
@@ -0,0 +1,33 @@
+const { DateTime } = require("luxon");
+
+module.exports = function(eleventyConfig) {
+  // Assets durchreichen
+  eleventyConfig.addPassthroughCopy("src/assets");
+
+  // Collection "episodes"
+  eleventyConfig.addCollection("episodes", function(collectionApi) {
+    return collectionApi.getFilteredByGlob("src/episodes/*.md");
+  });
+
+    // Datum-Filter
+  eleventyConfig.addFilter("date", (dateObj, format = "dd.MM.yyyy") => {
+    if (!dateObj || dateObj == "now"){
+      return DateTime.now().toFormat(format);
+    }
+    return DateTime.fromJSDate(dateObj).toFormat(format);
+  });
+
+  // RFC 2822
+  eleventyConfig.addFilter("rfc2822", (dateObj) => {
+    return DateTime.fromJSDate(dateObj).toRFC2822();
+  });
+
+  // 🔴 WICHTIG: URL-Filter
+  eleventyConfig.addFilter("url", function (url) {
+    const prefix = process.env.ELEVENTY_PATH_PREFIX || "/";
+    if (prefix === "/") {
+      return url;
+    }
+    return prefix.replace(/\/$/, "") + url;
+  });
+}
